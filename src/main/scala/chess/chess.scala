11366fba60a115b7b1e39a8d2c8be675824eb320
Mark Dobbins <mjdobbins12@gmail.com>
2020-01-21 22:10:00 +0000

    rooks are beginning to move...

--- src/main/scala/chess/chess.scala
+++ src/main/scala/chess/chess.scala
@@ -3,29 +3,34 @@ package chess
 sealed trait Piece {
 	def color: String
 	def position: Tuple2[Int, Int]
-	def legalMove: Boolean
+	def legalMove(target: Tuple2[Int, Int]): Boolean
 }
 
 final case class Pawn(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = ???
 }
 
 final case class Rook(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = target match {
+		case x if target == position => false
+		case x if target._1 == position._1 => true
+		case x if target._2 == position._2 => true
+		case _ => false
+	}
 }
 
 final case class Knight(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = ???
 }
 
 final case class Bishop(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = ???
 }
 
 final case class Queen(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = ???
 }
 
 final case class King(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove: Boolean = ???
+	def legalMove(target: Tuple2[Int, Int]): Boolean = ???
 }
--- src/test/scala/chess/chessSpec.scala
+++ src/test/scala/chess/chessSpec.scala
@@ -5,9 +5,16 @@ import org.scalatest._
 class ChessSpec extends FunSpec {
   describe("Pawn") {
     it("has a color and a location") {
-      val p = Pawn("white", (1, 2))
+      val p = Pawn("white", (0, 1))
       assert(p.color == "white")
-      assert(p.position == (1, 2))
+      assert(p.position == (0, 1))
+    }
+  }
+  
+  describe("Rook") {
+    it("is allowed to move laterally") {
+      val r = Rook("white", (0, 0))
+      assert(r.legalMove((0, 3))) == true
     }
   }
 }
