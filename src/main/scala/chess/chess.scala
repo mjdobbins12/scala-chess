d389b7ffc9ba4222a711b4ea4313be55853190a4
Mark Dobbins <mjdobbins12@gmail.com>
2020-02-03 22:00:33 +0000

    starting to simplify move functions

--- src/main/scala/chess/chess.scala
+++ src/main/scala/chess/chess.scala
@@ -4,6 +4,12 @@ sealed trait Piece {
 	def color: String
 	def position: Tuple2[Int, Int]
 	def legalMove(target: Tuple2[Int, Int]): Boolean
+	
+	def diagonalMotion(origin: Tuple2[Int, Int], target: Tuple2[Int, Int]): Boolean = target match {
+		case x if target == position => false
+		case x if (target._1 - position._1).abs == (target._2 - position._2).abs => true
+		case _ => false
+	}
 }
 
 final case class Pawn(color: String, position: Tuple2[Int, Int]) extends Piece {
@@ -29,10 +35,8 @@ final case class Knight(color: String, position: Tuple2[Int, Int]) extends Piece
 }
 
 final case class Bishop(color: String, position: Tuple2[Int, Int]) extends Piece {
-	def legalMove(target: Tuple2[Int, Int]): Boolean = target match {
-		case x if target == position => false
-		case x if (target._1 - position._1).abs == (target._2 - position._2).abs => true
-		case _ => false
+	def legalMove(target: Tuple2[Int, Int]): Boolean = {
+		diagonalMotion(position, target)
 	}
 }
 
